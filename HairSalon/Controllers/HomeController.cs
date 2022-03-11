5ebc397445752e45d0d662b564cff8662d72e635
Nick Sullivan <nicholassull@gmail.com>
2022-03-10 19:03:20 -0800

    add views for home and stylist index

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -1,1 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
+
+namespace HairSalon.Controllers
+{
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using HairSalon.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace HairSalon.Controllers
+{
+  public class StylistsController : Controller
+  {
+    private readonly HairSalonContext _db;
+    public StylistsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+      {
+        List<Stylist> model = _db.Stylists.ToList();
+        return View(model);
+      }
+  }
+}
