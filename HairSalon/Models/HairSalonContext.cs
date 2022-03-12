d356c4d7aa3e555ca94e691ee55764ff3db78e5e
Nick Sullivan <nicholassull@gmail.com>
2022-03-12 14:57:14 -0800

    remove comments

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -3,7 +3,6 @@ namespace HairSalon.Models
 {
   public class HairSalonContext : DbContext
   {
-    //Change to class the DB is dealing with
     public DbSet<Stylist> Stylists { get; set; }
     public DbSet<Client> Clients { get; set; }
     public HairSalonContext(DbContextOptions options) : base(options) { }
